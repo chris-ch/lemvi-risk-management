dbf86cca11fd309884e223b1ffa014f409664a68
Christophe <ch.alexandre@bluewin.ch>
2024-01-04 14:13:25 +0100

    dev

--- terraform/google-functions/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "function_source" {
-  value = data.archive_file.function_archives.output_path
-}
-
-output "function_uri" {
-  value = google_cloudfunctions2_function.function.service_config[0].uri
-}

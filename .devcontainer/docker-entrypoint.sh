d5665c67dc736bca2c6be486885868a44a0019ed
Christophe <ch.alexandre@bluewin.ch>
2024-01-05 09:21:26 +0000

    dev

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "executing entrypoint..."
+/home/appuser/.local/bin/poetry install --no-root --no-interaction --directory=/workspaces/lemvi-rm > poetry-install.log 2>&1
 
 exec "$@"
-

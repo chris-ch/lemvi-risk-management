fb7f747bb47711a8df7fb7445cff8ec76d02e40f
Christophe <chris-ch@users.noreply.github.com>
2024-01-05 15:02:38 +0000

    dev

--- .devcontainer/docker-entrypoint.sh
+++ .devcontainer/docker-entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "executing entrypoint..."
 /home/appuser/.local/bin/poetry install --no-root --no-interaction --directory=/workspaces/${RepositoryName} > poetry-install.log 2>&1
-export WORKSPACE_ENV_PATH=$(poetry env list | awk '/ / {print $1}')
+export WORKSPACE_ENV_PATH=$(poetry env list --directory=/workspaces/${RepositoryName} | awk '/ / {print $1}')
 exec "$@"
